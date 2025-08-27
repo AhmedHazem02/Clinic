5739007f098f3653be2d56df9ba83664f56afe0a
ali jad <jada2201075@gmail.com>
2025-08-27 11:54:39 +0000

    switch the places of these 2 elements

--- src/components/nurse/nurse-header.tsx
+++ src/components/nurse/nurse-header.tsx
@@ -11,14 +11,14 @@ export function NurseHeader() {
     return (
         <header className="sticky top-0 z-40 w-full border-b bg-background">
             <div className="container flex h-16 items-center space-x-4 px-4 sm:justify-between sm:space-x-0 sm:px-8">
-                <div className="flex flex-1 items-center justify-start space-x-4">
-                   <SignOutButton />
-                </div>
-                <div className="flex gap-6 md:gap-10">
+                <div className="flex flex-1 items-center justify-start gap-6 md:gap-10">
                     <Link href="/nurse/dashboard" className="flex items-center space-x-2">
                         <Stethoscope className="h-6 w-6 text-primary" />
                         <span className="inline-block font-bold">عيادة QueueWise</span>
                     </Link>
+                </div>
+                 <div className="flex items-center justify-end space-x-4">
+                   <SignOutButton />
                 </div>
             </div>
         </header>
